c56392ddea6d5f7cb95f3a42ff76d256ec325775
Celso Trabalho <celsortc@outlook.com>
2026-09-08 11:14:14 -0300

    feat: Now the transactions list updates as the data is inserted

--- modules/transactions.js
+++ modules/transactions.js
@@ -1,12 +1,12 @@
 export default function transactions() {
   const btnLancamento = document.querySelector(".btn-lancamento");
-  const btnCadastro = document.querySelector(".btn-cadastro");
+  const btnSalvar = document.querySelector(".btn-salvar");
   const forms = document.querySelector(".formulario-transactions");
   const transactionList = document.querySelector(".transactions-list");
   const transactions = [];
 
   btnLancamento.addEventListener("click", showForm);
-  btnCadastro.addEventListener("click", (e) => {
+  btnSalvar.addEventListener("click", (e) => {
     saveData(e);
   });
 
@@ -20,41 +20,68 @@ export default function transactions() {
     const formsDescricao = document.getElementById("descricao-forms").value;
     const formsCategoria = document.getElementById("categorias-forms").value;
     const formsValor = document.getElementById("valor-forms").value;
+    const formsData = document.getElementById("data-forms").value;
+    let symbol;
+    let symbolClass;
+
+    if (formsCategoria !== "salario") {
+      symbol = "-";
+    } else symbol = "+";
+    const tipoTransacao = symbol === "-" ? "expense" : "income";
 
     const novaTransacao = {
       descricao: formsDescricao,
       categoria: formsCategoria,
       valor: formsValor,
+      op: symbol,
+      tipo: tipoTransacao,
+      data: formsData,
     };
+    console.log(typeof +(novaTransacao.op + novaTransacao.valor));
 
-    const li = document.createElement("li");
-    const spanDescricao = document.createElement("span");
-    spanDescricao.innerText = formsDescricao;
-    spanDescricao.classList.add("transaction-title");
+    transactions.push(novaTransacao);
+    showData();
+    forms.reset();
+  }
 
-    const spanCategoria = document.createElement("span");
-    spanCategoria.innerText = formsCategoria;
-    spanCategoria.classList.add("transaction-category");
+  function showData(symbolClass) {
+    console.log(transactions);
 
-    const spanSymbol = document.createElement("span");
-    if (spanCategoria !== "Salário") {
-      spanSymbol.innerText = "-";
-      spanSymbol.classList.add("expense");
-    }
+    transactionList.innerHTML = "";
 
-    const spanValor = document.createElement("span");
-    spanValor.appendChild(spanSymbol);
-    spanValor.innerText += ` R$ ${formsValor},00`;
-    spanValor.classList.add("transaction-value");
+    transactions.forEach((transaction) => {
+      const li = document.createElement("li");
+      li.classList.add("transaction-item");
 
-    const elementos = [spanDescricao, spanCategoria, spanValor];
-    elementos.forEach((elemento) => {
-      li.appendChild(elemento);
+      li.innerHTML = `
+      <span class="transaction-title">${transaction.descricao}</span>
+      <span class="transaction-category">${transaction.categoria}</span>
+      <span class="transaction-value"><span class="${transaction.tipo}">${transaction.op}\t</span>${transaction.valor}</span>
+      `;
+
+      transactionList.appendChild(li);
     });
 
-    li.classList.add("transaction-item");
-    transactionList.appendChild(li);
+    // const li = document.createElement("li");
+    // const spanDescricao = document.createElement("span");
+    // spanDescricao.innerText = newTransaction.descricao;
+    // spanDescricao.classList.add("transaction-title");
 
-    console.log(novaTransacao);
+    // const spanCategoria = document.createElement("span");
+    // spanCategoria.innerText = newTransaction.categoria;
+    // spanCategoria.classList.add("transaction-category");
+
+    // const spanValor = document.createElement("span");
+    // spanValor.appendChild(spanSymbol);
+    // spanValor.innerText += ` R$ ${newTransaction.valor},00`;
+    // spanValor.classList.add("transaction-value");
+
+    // const elementos = [spanDescricao, spanCategoria, spanValor];
+    // elementos.forEach((elemento) => {
+    //   li.appendChild(elemento);
+    // });
+
+    // li.classList.add("transaction-item");
+    // transactionList.appendChild(li);
   }
 }
