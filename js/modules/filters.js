336e4493dc297fbb1b8ad577b565b708068cbc61
Celso Trabalho <celsortc@outlook.com>
2026-09-18 11:17:18 -0300

    feat(dropdown): show menu on click and hover

--- js/modules/filters.js
+++ js/modules/filters.js
@@ -1,7 +1,5 @@
 export default function filters() {
-  const filters = document.querySelectorAll(".filters li");
-
-  console.log(filters);
+  const filters = document.querySelectorAll(".filters [data-dropdown]");
 
   filters.forEach((e) => {
     console.log(e);
@@ -10,6 +8,13 @@ export default function filters() {
   });
 
   function activeBtn(event) {
+    const currentFilterSpan = event.currentTarget;
+
+    filters.forEach((btn) => {
+      if (btn !== currentFilterSpan) {
+        btn.classList.remove("active");
+      }
+    });
     event.currentTarget.classList.toggle("active");
   }
 }
