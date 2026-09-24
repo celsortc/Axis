3b70faf290229deb623299a8e1fc67b3aacfb121
Celso Toledo <105131736+celsortc@users.noreply.github.com>
2026-09-23 21:48:44 -0300

    feat(filters): filter transactions by type

--- js/modules/filters.js
+++ js/modules/filters.js
@@ -1,35 +1,22 @@
-// import { transactions } from "./transactions.js";
+import { transactions } from "./transactions.js";
 
-export default function filters() {
+export default function filters(showData) {
   const filterBtns = document.querySelectorAll("[data-dropdown] .filter-btn");
+  const typeCheckboxes = document.querySelectorAll(".typeCheckbox");
 
   filterBtns.forEach((e) => {
     e.addEventListener("click", activeBtn);
   });
 
-  // const typeCheckboxes = document.querySelectorAll(".typeCheckbox");
-
-  // typeCheckboxes.forEach((checkBox) => {
-  //   checkBox.addEventListener("change", filterTransactions);
-  // });
-
-  // const filterTypeOptions = document.querySelectorAll(".dropdown-menu li");
-
-  // filterTypeOptions.forEach((option) => {
-  //   option.addEventListener("click", () => {
-  //     filterTransactions(option.dataset.typeFilter);
-  //   });
-  // });
+  typeCheckboxes.forEach((checkBox) => {
+    checkBox.addEventListener("change", filterTransactions);
+  });
 
   function activeBtn(event) {
-    // Encontra o menu associado ao botão clicado
     const currentDropdown = this.closest("li").querySelector(".dropdown-menu");
     removeActive(currentDropdown);
 
-    //pega o valor do  data-dropdown da categoria de filtro clicado (tipo, categoria ou período)
     const targetId = this.dataset.target;
-
-    //busca a ul (dropdown menu) com ID identico ao targetID
     const dropdown = document.getElementById(targetId);
 
     dropdown.classList.toggle("active");
@@ -42,8 +29,8 @@ export default function filters() {
   });
 
   function removeActive(cDropdown = null) {
-    // Busca TODOS os dropdowns da página
     const allDropdowns = document.querySelectorAll(".dropdown-menu");
+
     allDropdowns.forEach((dropdown) => {
       if (dropdown !== cDropdown) {
         dropdown.classList.remove("active");
@@ -51,16 +38,15 @@ export default function filters() {
     });
   }
 
-  // function filterTransactions(event) {
-  //   // const filtered = transactions.filter((transaction) => {
-  //   const checkbox = event.currentTarget;
+  function filterTransactions() {
+    const selectedTypes = [...typeCheckboxes]
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => checkbox.dataset.typefilter);
 
-  //   console.log(checkbox.dataset.typefilter);
-  //   console.log(checkbox.checked);
+    const filtered = transactions.filter((transaction) =>
+      selectedTypes.includes(transaction.tipo),
+    );
 
-  //   // return transaction.tipo === type;
-  //   // });
-
-  //   console.log(filtered);
-  // }
+    showData(filtered);
+  }
 }
